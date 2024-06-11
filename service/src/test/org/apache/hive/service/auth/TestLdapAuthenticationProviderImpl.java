36303b00b0167ed9ad2226fba0fc7b68d0c05910
Christos Bisias <christosbis@gmail.com>
2024-06-11 19:34:39 +0300

    cleanup

--- service/src/test/org/apache/hive/service/auth/TestLdapAuthenticationProviderImpl.java
+++ service/src/test/org/apache/hive/service/auth/TestLdapAuthenticationProviderImpl.java
@@ -221,10 +221,7 @@ public class TestLdapAuthenticationProviderImpl {
 
     when(search.findUserDn("user3")).thenReturn("cn=user3,ou=PowerUsers,dc=mycorp,dc=com");
 
-//    Unnecessary stubbings detected in test class: TestLdapAuthenticationProviderImpl
-//    Clean & maintainable test code requires zero unnecessary code.
-//    Following stubbings are unnecessary
-
+//    This line causes a mockito UnnecessaryStubbingException.
 //    when(search.findGroupsForUser("cn=user3,ou=PowerUsers,dc=mycorp,dc=com"))
 //        .thenReturn(Arrays.asList(
 //            "cn=testGroup,ou=Groups,dc=mycorp,dc=com",
@@ -255,10 +252,7 @@ public class TestLdapAuthenticationProviderImpl {
         "(&(objectClass=person)(|(memberOf=CN=Domain Admins,CN=Users,DC=apache,DC=org)(memberOf=CN=Administrators,CN=Builtin,DC=apache,DC=org)))");
     conf.setVar(HiveConf.ConfVars.HIVE_SERVER2_PLAIN_LDAP_USERFILTER, "user3");
 
-//    Unnecessary stubbings detected in test class: TestLdapAuthenticationProviderImpl
-//    Clean & maintainable test code requires zero unnecessary code.
-//    Following stubbings are unnecessary
-
+//    This line causes a mockito UnnecessaryStubbingException.
 //    when(search.findUserDn("user3")).thenReturn("cn=user3,ou=PowerUsers,dc=mycorp,dc=com");
     when(search.executeCustomQuery(anyString())).thenReturn(Arrays.asList(
         "cn=user1,ou=PowerUsers,dc=mycorp,dc=com",
