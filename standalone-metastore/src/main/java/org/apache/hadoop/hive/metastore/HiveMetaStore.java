bef77533f986c1f465a3595e1bcebe1aa5219317
Christos Bisias <christosbis@gmail.com>
2024-07-11 20:40:42 +0300

    Don't allow the HiveMetastore service to start if it's configured in local/embedded mode (#10)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -8848,6 +8848,16 @@ public class HiveMetaStore extends ThriftHiveMetastore {
         }
       }
 
+      String msUri = MetastoreConf.getVar(conf, ConfVars.THRIFT_URIS);
+      // Checks whether msUri is null or empty.
+      // It doesn't check whether the msUri is valid.
+      boolean localMetaStore = MetastoreConf.isEmbeddedMetaStore(msUri);
+      if (localMetaStore) {
+        throw new MetaException("Local/Embedded metastore is not allowed. Please configure '"
+            + ConfVars.THRIFT_URIS.getVarname() + "' or '" + ConfVars.THRIFT_URIS.getHiveName() +
+            "'; it is currently set to [" + msUri + "]");
+      }
+
       Lock startLock = new ReentrantLock();
       Condition startCondition = startLock.newCondition();
       AtomicBoolean startedServing = new AtomicBoolean();
